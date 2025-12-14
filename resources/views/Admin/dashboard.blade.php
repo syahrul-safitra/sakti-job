731fd7a466109443864aecd571d331fb87e4bd96
syahrul-safitra <syahrulsafitra7@gmail.com>
2025-12-14 19:52:30 +0700

    perbaiki sidebar admin

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="page-content">
         <section class="row">
-            <div class="col-12 col-lg-9">
+            <div class="col-12">
                 <div class="row">
                     <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
@@ -93,7 +93,7 @@
                     </div>
                 </div>
 
-                <div class="row">
+                {{-- <div class="row">
                     <div class="col-12 col-xl-4">
                         <div class="card">
                             <div class="card-header">
@@ -205,10 +205,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
 
-            <div class="col-12 col-lg-3">
+            {{-- <div class="col-12 col-lg-3">
                 <div class="card">
                     <div class="card-body px-4 py-4">
                         <div class="d-flex align-items-center">
@@ -268,7 +268,7 @@
                         <div id="chart-visitors-profile"></div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </section>
     </div>
 
